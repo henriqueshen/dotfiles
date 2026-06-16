99c21993bad58afaaa877fe32b66b37d8741b3d8
Henrique Shen <hshen2908@gmail.com>
2026-06-15 19:18:12 -0700

    fic keyboard and enable bt

--- modules/hosts/laptop/configuration.nix
+++ modules/hosts/laptop/configuration.nix
@@ -12,70 +12,51 @@
         self.nixosModules.niri
       ];
 
-      # Bootloader.
-      boot.loader.systemd-boot.enable = true;
-      boot.loader.efi.canTouchEfiVariables = true;
-
-      networking.hostName = "nixos"; # Define your hostname.
-      # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-      # Configure network proxy if necessary
-      # networking.proxy.default = "http://user:password@proxy:port/";
-      # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-      # Enable networking
-      networking.networkmanager.enable = true;
-
-      # Set your time zone.
-      time.timeZone = "America/Sao_Paulo";
-
-      # Select internationalisation properties.
-      i18n.defaultLocale = "en_US.UTF-8";
-
-      i18n.extraLocaleSettings = {
-        LC_ADDRESS = "pt_BR.UTF-8";
-        LC_IDENTIFICATION = "pt_BR.UTF-8";
-        LC_MEASUREMENT = "pt_BR.UTF-8";
-        LC_MONETARY = "pt_BR.UTF-8";
-        LC_NAME = "pt_BR.UTF-8";
-        LC_NUMERIC = "pt_BR.UTF-8";
-        LC_PAPER = "pt_BR.UTF-8";
-        LC_TELEPHONE = "pt_BR.UTF-8";
-        LC_TIME = "pt_BR.UTF-8";
+      boot = {
+        loader = {
+          limine = {
+            enable = true;
+            efiSupport = true;
+          };
+          systemd-boot.enable = false;
+          efi.canTouchEfiVariables = true;
+        };
+        initrd.availableKernelModules = [
+          "usbhid"
+          "xhci_pci"
+          "nvme"
+          "usb_storage"
+          "sd_mod"
+        ];
       };
 
-      # Configure keymap in X11
+      networking.hostName = "nixos";
+
+      networking.networkmanager.enable = true;
+
+      time.timeZone = "America/Sao_Paulo";
+
+      i18n.defaultLocale = "en_US.UTF-8";
+
       services.xserver.xkb = {
         layout = "us";
         variant = "";
       };
 
-      # Some programs need SUID wrappers, can be configured further or are
-      # started in user sessions.
-      # programs.mtr.enable = true;
-      # programs.gnupg.agent = {
-      #   enable = true;
-      #   enableSSHSupport = true;
-      # };
+      hardware.bluetooth = {
+        enable = true;
+        powerOnBoot = true;
+      };
 
-      # List services that you want to enable:
+      programs.mtr.enable = true;
+      programs.gnupg.agent = {
+        enable = true;
+        enableSSHSupport = true;
+      };
 
-      # Enable the OpenSSH daemon.
       services.openssh.enable = true;
 
-      # Open ports in the firewall.
-      # networking.firewall.allowedTCPPorts = [ ... ];
-      # networking.firewall.allowedUDPPorts = [ ... ];
-      # Or disable the firewall altogether.
-      # networking.firewall.enable = false;
-
-      # This value determines the NixOS release from which the default
-      # settings for stateful data, like file locations and database versions
-      # on your system were taken. It‘s perfectly fine and recommended to leave
-      # this value at the release version of the first install of this system.
-      # Before changing this value read the documentation for this option
-      # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-      system.stateVersion = "26.05"; # Did you read the comment?
+      system.stateVersion = "26.05";
 
     };
 }
