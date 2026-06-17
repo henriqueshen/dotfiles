01535283c648a4b45b9e2874865bb558ac7248f0
Henrique Shen <hshen2908@gmail.com>
2026-06-17 13:40:00 -0700

    1

--- modules/common/secrets.nix
+++ modules/common/secrets.nix
@@ -5,13 +5,13 @@
       imports = [ inputs.sops-nix.nixosModules.sops ];
 
       # Global sops-nix configuration shared across all hosts using this module
-      sops = {
-        defaultSopsFile = ../secrets/secrets.yaml; # Adjust path to your encrypted sops file
-        defaultSopsFormat = "yaml";
-
-        # Automatically use the machine's SSH key for decryption
-        age.keyFile = "/var/lib/sops-nix/key.txt";
-        sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-      };
+      # sops = {
+      #   defaultSopsFile = ../secrets/secrets.yaml; # Adjust path to your encrypted sops file
+      #   defaultSopsFormat = "yaml";
+      #
+      #   # Automatically use the machine's SSH key for decryption
+      #   age.keyFile = "/var/lib/sops-nix/key.txt";
+      #   sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+      # };
     };
 }
