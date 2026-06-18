3066d209838b65209aaf9441664580f68737142c
Henrique Shen <hshen2908@gmail.com>
2026-06-17 20:48:22 -0700

    fix git

--- modules/apps/git/app.nix
+++ modules/apps/git/app.nix
@@ -7,24 +7,6 @@
 
       programs.git = {
         enable = true;
-        startAgent = true;
-        extraConfig = ''
-          AddKeysToAgent yes
-        '';
-        settings.user = {
-          name = "Henrique Shen";
-          email = "dev.henrique.shen@gmail.com";
-        };
-      };
-
-      services.ssh-agent.enable = true;
-      programs.ssh = {
-        enable = true;
-        matchBlocks = {
-          "*" = {
-            addKeysToAgent = "yes";
-          };
-        };
       };
     };
 }
