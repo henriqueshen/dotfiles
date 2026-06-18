8fcab5bb0e46ca4e55d605e1d2b28f73afa211d1
Henrique Shen <hshen2908@gmail.com>
2026-06-17 21:10:44 -0700

    disable host secret

--- modules/users/hshen2908/secrets.nix
+++ modules/users/hshen2908/secrets.nix
@@ -2,10 +2,10 @@
   flake.nixosModules.hshen2908Secrets =
     { config, pkgs, ... }:
     {
-      imports = [ self.nixosModules.secretsCommon ];
-
-      sops.secrets."hshen2908/sops/age-key" = {
-        owner = "hshen2908";
-      };
+      # imports = [ self.nixosModules.secretsCommon ];
+      #
+      # sops.secrets."hshen2908/sops/age-key" = {
+      #   owner = "hshen2908";
+      # };
     };
 }
