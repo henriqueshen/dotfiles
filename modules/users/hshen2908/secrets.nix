bcec6ba86f5928ff6fecd2023322a0ca7c3a3330
Henrique Shen <hshen2908@gmail.com>
2026-06-17 22:22:10 -0700

    update secret recipients

--- modules/users/hshen2908/secrets.nix
+++ modules/users/hshen2908/secrets.nix
@@ -2,10 +2,11 @@
   flake.nixosModules.hshen2908Secrets =
     { config, pkgs, ... }:
     {
-      # imports = [ self.nixosModules.secretsCommon ];
-      #
-      # sops.secrets."hshen2908/sops/age-key" = {
-      #   owner = "hshen2908";
-      # };
+      imports = [ self.nixosModules.secretsCommon ];
+
+      sops.secrets."hshen2908/sops/age-key" = {
+        owner = "hshen2908";
+        path = "${config.home-manager.users.hshen2908.home.homeDirectory}/.config/sops/age/keys.txt";
+      };
     };
 }
