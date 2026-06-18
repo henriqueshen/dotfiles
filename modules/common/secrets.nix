6fdfcdebe0d5b5a6271f2d43b13e7311e961af03
Henrique Shen <hshen2908@gmail.com>
2026-06-17 22:32:04 -0700

    fix secret path

--- modules/common/secrets.nix
+++ modules/common/secrets.nix
@@ -5,7 +5,7 @@
       imports = [ inputs.sops-nix.nixosModules.sops ];
 
       sops = {
-        defaultSopsFile = ../secrets/secrets.yaml;
+        defaultSopsFile = ../../secrets/secrets.yaml;
         defaultSopsFormat = "yaml";
 
         age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
