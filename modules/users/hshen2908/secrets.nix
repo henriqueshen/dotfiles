cd94f2fb2f25735c71190bb1fff0fda12e982336
Henrique Shen <hshen2908@gmail.com>
2026-06-17 22:28:03 -0700

    fix secret path

--- modules/users/hshen2908/secrets.nix
+++ modules/users/hshen2908/secrets.nix
@@ -4,7 +4,7 @@
     {
       imports = [ self.nixosModules.secretsCommon ];
 
-      sops.secrets."hshen2908/sops/age-key" = {
+      sops.secrets."users/hshen2908/sops/age-key" = {
         owner = "hshen2908";
         path = "${config.home-manager.users.hshen2908.home.homeDirectory}/.config/sops/age/keys.txt";
       };
