dfbfa8617a81b30c4966cbae88459a18d3279151
Henrique Shen <hshen2908@gmail.com>
2026-06-17 22:29:21 -0700

    1

--- modules/users/hshen2908/secrets.nix
+++ modules/users/hshen2908/secrets.nix
@@ -6,7 +6,7 @@
 
       sops.secrets."users/hshen2908/sops/age-key" = {
         owner = "hshen2908";
-        path = "${config.home-manager.users.hshen2908.home.homeDirectory}/.config/sops/age/keys.txt";
+        # path = "${config.home-manager.users.hshen2908.home.homeDirectory}/.config/sops/age/keys.txt";
       };
     };
 }
