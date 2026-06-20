b1cda1893c816f035aa6d6ef212f93f3c51356fc
Henrique Shen <hshen2908@gmail.com>
2026-06-19 19:38:46 -0700

    configure formatter and linter

--- modules/apps/neovim/nixvim/formatter/conform-nvim.nix
+++ modules/apps/neovim/nixvim/formatter/conform-nvim.nix
@@ -3,12 +3,15 @@
   flake.nixvimModules.conform-nvim = { pkgs, lib, ... }: {
     plugins.conform-nvim = {
       enable = true;
+      autoInstall.enable = true;
       settings = {
         format_on_save = {
           lsp_format = "fallback";
           timeout_ms = 500;
         };
       };
+      formatters = { };
+      formatter_by_ft = { };
     };
   };
 }
--- modules/apps/neovim/nixvim/linter/lint.nix
+++ modules/apps/neovim/nixvim/linter/lint.nix
@@ -3,12 +3,18 @@
   flake.nixvimModules.lint = { pkgs, lib, ... }: {
     plugins.lint = {
       enable = true;
-      settings = {
-        format_on_save = {
-          lsp_format = "fallback";
-          timeout_ms = 500;
+      autoInstall.enable = true;
+      autoCmd = {
+        callback = {
+          __raw = ''
+            function()
+              require('lint').try_lint()
+            end
+          '';
         };
+        event = "BufWritePost";
       };
+      lintersByFt = { };
     };
   };
 }
