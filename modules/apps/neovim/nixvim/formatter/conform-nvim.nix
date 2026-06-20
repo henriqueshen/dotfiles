5faccb24eff9512a22f6d5ebcd13aba58686cfa1
Henrique Shen <hshen2908@gmail.com>
2026-06-19 19:43:01 -0700

    fix formatter

--- modules/apps/neovim/nixvim/formatter/conform-nvim.nix
+++ modules/apps/neovim/nixvim/formatter/conform-nvim.nix
@@ -3,7 +3,7 @@
   flake.nixvimModules.conform-nvim = { pkgs, lib, ... }: {
     plugins.conform-nvim = {
       enable = true;
-      autoInstall.enable = true;
+      # autoInstall.enable = true;
       settings = {
         format_on_save = {
           lsp_format = "fallback";
