e3a07e5e019b9671bfa02dba5a841465319b5344
Henrique Shen <hshen2908@gmail.com>
2026-06-19 19:31:27 -0700

    fix lint plugin

--- modules/apps/neovim/nixvim/linter/lint.nix
+++ modules/apps/neovim/nixvim/linter/lint.nix
@@ -1,7 +1,7 @@
 { self, inputs, ... }:
 {
-  flake.nixvimModules.conform-nvim = { pkgs, lib, ... }: {
-    plugins.conform-nvim = {
+  flake.nixvimModules.lint = { pkgs, lib, ... }: {
+    plugins.lint = {
       enable = true;
       settings = {
         format_on_save = {
