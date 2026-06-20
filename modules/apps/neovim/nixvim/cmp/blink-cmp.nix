bbf64c839a936be051d4f1f08d2c9a24659a7f8c
Henrique Shen <hshen2908@gmail.com>
2026-06-19 19:02:08 -0700

    fix cmp keys

--- modules/apps/neovim/nixvim/cmp/blink-cmp.nix
+++ modules/apps/neovim/nixvim/cmp/blink-cmp.nix
@@ -21,18 +21,84 @@
           documentation = {
             auto_show = true;
           };
+          ghost_text = {
+            enabled = true;
+          };
         };
         keymap = {
-          preset = "super-tab";
+          preset = "default";
+          "<C-d>" = [
+            "show"
+            "show_documentation"
+            "hide_documentation"
+          ];
+          "<C-e>" = [
+            "hide"
+            "fallback"
+          ];
+          "<CR>" = [
+            "select_and_accept"
+            "fallback"
+          ];
+
+          "<Up>" = [
+            "select_prev"
+            "fallback"
+          ];
+          "<Down>" = [
+            "select_next"
+            "fallback"
+          ];
+          "<C-k>" = [
+            "select_prev"
+            "fallback_to_mappings"
+          ];
+          "<C-j>" = [
+            "select_next"
+            "fallback_to_mappings"
+          ];
+
+          "<C-h>" = [
+            "scroll_documentation_up"
+            "fallback"
+          ];
+          "<C-l>" = [
+            "scroll_documentation_down"
+            "fallback"
+          ];
+
+          "<Tab>" = [
+            "snippet_forward"
+            lib.nixvim.mkRaw
+            "function() -- if you are using Neovim's native inline completions
+						return vim.lsp.inline_completion.get()
+					end"
+            "fallback"
+          ];
+          "<S-Tab>" = [
+            "snippet_backward"
+            "fallback"
+          ];
+
+          "<C-s>" = [
+            "show_signature"
+            "hide_signature"
+            "fallback"
+          ];
         };
+
         signature = {
           enabled = true;
+        };
+        fuzzy = {
+          implementation = "prefer_rust_with_warning";
         };
       };
 
       settings.sources.default = [
         "lsp"
         "path"
+        "snippets"
         "buffer"
         "git"
       ];
