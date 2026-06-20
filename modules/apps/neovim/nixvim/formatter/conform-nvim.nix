949b9ae3367ab0fa20536c16fde45ce674c92e18
Henrique Shen <hshen2908@gmail.com>
2026-06-19 19:39:17 -0700

    fix formatter

--- modules/apps/neovim/nixvim/formatter/conform-nvim.nix
+++ modules/apps/neovim/nixvim/formatter/conform-nvim.nix
@@ -9,9 +9,9 @@
           lsp_format = "fallback";
           timeout_ms = 500;
         };
+        formatters = { };
+        formatter_by_ft = { };
       };
-      formatters = { };
-      formatter_by_ft = { };
     };
   };
 }
