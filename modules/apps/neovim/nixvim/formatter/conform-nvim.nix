2a7345684e121a4db322b2f8953015fbd559da0b
Henrique Shen <hshen2908@gmail.com>
2026-06-19 19:51:37 -0700

    fix formatter

--- modules/apps/neovim/nixvim/formatter/conform-nvim.nix
+++ modules/apps/neovim/nixvim/formatter/conform-nvim.nix
@@ -3,15 +3,16 @@
   flake.nixvimModules.conform-nvim = { pkgs, lib, ... }: {
     plugins.conform-nvim = {
       enable = true;
-      # autoInstall.enable = true;
+      autoInstall.enable = true;
       settings = {
         format_on_save = {
           lsp_format = "fallback";
           timeout_ms = 500;
         };
-        # formatters = {
-        #
-        # };
+        formatters_by_ft = {
+          nix = [ "nixfmt" ];
+          lua = [ "lua-format" ];
+        };
       };
     };
   };
