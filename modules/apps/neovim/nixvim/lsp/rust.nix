4809b39accb88e0699761aa9f5750bf853e466fb
Henrique Shen <dev.henrique.shen@gmail.com>
2026-06-21 00:47:39 -0300

    fix rust

--- modules/apps/neovim/nixvim/lsp/rust.nix
+++ modules/apps/neovim/nixvim/lsp/rust.nix
@@ -1,16 +1,22 @@
 { self, inputs, ... }:
 {
   flake.nixvimModules.lsp-rust = { pkgs, lib, ... }: {
+    extraPackages = with pkgs; [
+      rust-analyzer
+      cargo
+      rustc
+    ];
+
     plugins.rustaceanvim = {
       enable = true;
       settings = {
         server = {
-          cmd = [
-            "rustup"
-            "run"
-            "nightly"
-            "rust-analyzer"
-          ];
+          # cmd = [
+          #   "rustup"
+          #   "run"
+          #   "nightly"
+          #   "rust-analyzer"
+          # ];
           default_settings = {
             rust-analyzer = {
               cargo = {
